5e771e9ddad5df0bbbb26255e02266841b5d235b
Mauro Casadei <info@soluzionesoftware.com>
2026-06-10 15:02:33 +0200

    lez1

---
+++ lezione1/app-catalogo/src/products/products.controller.ts
@@ -0,0 +1,36 @@
+import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+
+@Controller('products') //localhost:3000/products
+export class ProductsController {
+    @Get()
+    getProducts(): string {
+        return 'Elenco dei prodotti';
+    }
+
+    @Get('search')
+    searchProducts(): string {
+        return 'Ricerca dei prodotti';
+    }
+
+    @Get(':id') //localhost:3000/products/123
+    getProductById(@Param('id') id: string): string {
+        return 'Dettagli del prodotto:' + ` ${id}`; //stringa dinamica
+    }
+
+    @Post()
+    createProduct(): string {
+        return 'Creazione di un nuovo prodotto';
+    }
+
+    @Put(':id')
+    updateProduct(@Param('id') id: string): string {
+        return 'Aggiornamento del prodotto:' + `${id}`; //stringa dinamica  
+    }
+
+    @Delete(':id')
+    deleteProduct(@Param('id') id: string): string {
+        return 'Eliminazione del prodotto:' + `${id}`; //stringa dinamica
+    }
+
+
+}
