0a223242330bef2e3fc8eb7b77d47676a89d3c2e
Mauro Casadei <info@soluzionesoftware.com>
2026-06-17 13:40:40 +0200

    lez 3.3

--- app-catalogo3/src/app.module.ts
+++ app-catalogo3/src/app.module.ts
@@ -1,8 +1,9 @@
-import { Module } from '@nestjs/common';
+import { Module, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { AuthMiddleware } from './auth/auth.middleware';
+import { TolowerMiddleware } from './tolower/tolower.middleware';
 
 @Module({
   imports: [ProductsModule],
@@ -11,8 +12,12 @@ import { AuthMiddleware } from './auth/auth.middleware';
 })
 export class AppModule {
   configure(consumer: any) {
-    console.log('app.module.ts: 1 start ');
+    // console.log('app.module.ts: 1 start ');
     consumer.apply(AuthMiddleware).forRoutes('*');
+    consumer.apply(TolowerMiddleware).forRoutes(
+      { path: 'products', method: RequestMethod.POST },
+      { path: 'products/:id', method: RequestMethod.PUT },
+    );
     console.log('app.module.ts: 2 auth middleware applied ');
   }
 }
