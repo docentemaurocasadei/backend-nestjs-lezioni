ddbc234458e3198d12dfb9a262516232cb20df0b
Mauro Casadei <info@soluzionesoftware.com>
2026-06-24 10:38:53 +0200

    catalogo 4

---
+++ app-catalogo4/src/products/products.service.ts
@@ -0,0 +1,30 @@
+import { Injectable, Query } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+
+@Injectable()
+export class ProductsService {
+  // create(createProductDto: CreateProductDto) {
+  //   return 'This action adds a new product';
+  // }
+
+  findAll() {
+    return `This action returns all products`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} product`;
+  }
+
+  search(p: string) {
+    return `This action returns a #${p} product`;
+  }
+
+  // update(id: number, updateProductDto: UpdateProductDto) {
+  //   return `This action updates a #${id} product`;
+  // }
+
+  // remove(id: number) {
+  //   return `This action removes a #${id} product`;
+  // }
+}
